3535e930b5cd92b979dd21e8815973121971f07a
Paul Richardson <developer@photographerafoot.com>
2021-06-07 08:18:57 +1000

    CHG: create view

--- Extensions/ViewController.swift
+++ Extensions/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet var blob: UIView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
 
+		blob = UIView()
+		blob.backgroundColor = .red
+		blob.layer.cornerRadius = 25.0
+		blob.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(blob)
+
+		let constraints = [
+			blob.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+			blob.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+			blob.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+			blob.heightAnchor.constraint(equalTo: blob.widthAnchor, multiplier: 1)
+		]
+		NSLayoutConstraint.activate(constraints)
+
+	}
 
 }
 
