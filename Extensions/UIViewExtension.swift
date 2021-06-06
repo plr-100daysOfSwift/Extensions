820813f4e839e51ffd541f417571ab29054b5c92
Paul Richardson <developer@photographerafoot.com>
2021-06-07 08:43:35 +1000

    CHG: extend UIView with bounceOut(withDuration:)
    bounceOut animates a scaledBy transform

---
+++ Extensions/UIViewExtension.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewExtension.swift
+//  Extensions
+//
+//  Created by Paul Richardson on 07/06/2021.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+
+	func bounceOut(withDuration duration: TimeInterval) {
+		let delay = 2.0
+		let scaleFactor: CGFloat = 0.001
+		UIView.animate(withDuration: duration, delay: delay) {
+			self.transform = self.transform.scaledBy(x: scaleFactor, y: scaleFactor)
+		}
+	}
+
+}
