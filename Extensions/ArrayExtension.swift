5ddd4af0cab5480ec443a124bcc3faa7cdc7c1cf
Paul Richardson <developer@photographerafoot.com>
2021-06-07 10:24:00 +1000

    CHG: use firstIndex(of:)
    firstIndex(where:) is unnecessarily complicated

--- Extensions/ArrayExtension.swift
+++ Extensions/ArrayExtension.swift
@@ -9,10 +9,8 @@ import Foundation
 
 extension Array where Element: Comparable {
 
-	mutating func remove(item: Any) {
-		if let index = self.firstIndex(where: { element in
-			element == item as! Element
-		}) {
+	mutating func remove(item: Element) {
+		if let index = self.firstIndex(of: item) {
 			self.remove(at: index)
 		}
 	}
