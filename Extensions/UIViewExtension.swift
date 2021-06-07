938a49ce8512377b95d476589c58881e497d665d
Paul Richardson <developer@photographerafoot.com>
2021-06-07 10:20:31 +1000

    CHG: add capture list

--- Extensions/UIViewExtension.swift
+++ Extensions/UIViewExtension.swift
@@ -13,7 +13,7 @@ extension UIView {
 	func bounceOut(withDuration duration: TimeInterval) {
 		let delay = 2.0
 		let scaleFactor: CGFloat = 0.001
-		UIView.animate(withDuration: duration, delay: delay) {
+		UIView.animate(withDuration: duration, delay: delay) { [unowned self] in
 			self.transform = self.transform.scaledBy(x: scaleFactor, y: scaleFactor)
 		}
 	}
